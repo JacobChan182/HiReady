5d877c1d258c1d6d0a11003378a8106d3cf55372
xaderF <ryanyu365@gmail.com>
2026-01-17 12:01:42 -0500

    small fixes on styling and some errors.

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import path from 'path';
-import { fileURLToPath } from 'url'; // This is what was missing
+import { fileURLToPath } from 'url'; 
 
 // These two lines recreate __dirname for ES Modules
 const __filename = fileURLToPath(import.meta.url);
@@ -23,11 +23,9 @@ import uploadRoutes from './routes/upload';
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Middleware
 app.use(cors());
 app.use(express.json({ limit: '500mb' }));
 
-// Connect to MongoDB
 connectDB().catch(console.error);
 
 // Routes
